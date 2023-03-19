beedb5d01fe7c55053f5b1644d97dfe5b827ab9f
Masato <masato_7121@keio.jp>
2023-03-19 15:47:49 +0900

    Done: main loop for detection

---
+++ AR_proto/ar_module.py
@@ -0,0 +1,115 @@
+import numpy as np
+import cv2
+from cv2 import aruco
+import sys
+import time
+import datetime
+
+class Ar_cansat():
+    """ Class for detect AR marker and position
+
+    """
+    marker_length = 0.01 # [m]
+    # マーカーの辞書選択
+    dictionary = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)  #ARマーカーの生成に使用
+    #aruco.customDictionary(nMakers(ID数),Markersize,baseDictionary(基本となるディクショナリ))で独自のディクショナリ作成も可能
+
+    #レンズの性質などの内部パラメータ(今回はすでに行っている)
+    camera_matrix = np.load("mtx.npy")
+    distortion_coeff = np.load("dist.npy")
+
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+
+    def capture(self, args):
+        """
+        引数0→写真
+        引数1→動画
+        """
+        # print(cap.get(0))  # "CAP_PROP_FRAME_WIDTH"
+        # cap.set(cv2.CAP_PROP_AUTOFOCUS,0.0)
+        # print(cap.get(cv2.CAP_PROP_AUTOFOCUS))
+        # print(cap.get())
+
+        self.ret,self.img = self.cap.read()
+        if args == 0:
+            now = datetime.datetime.now()
+            now = now.strftime('%Y%m%d%H%M%S')
+            cv2.imwrite(f"pics/{now}.jpg", self.img)
+            # print(ret)
+            # print(now)
+            return self.img
+        elif args == 1:
+            # cv2.imshow("realtime", self.img)
+            return self.img
+        else:
+            return None
+
+    def detect_marker(self, img):
+        print(img)
+                            #使用するARマーカーのライブラリ、マーカーの大きさは不変であるため宣言しておく必要あり
+        ar_info = []
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(img, self.dictionary)
+        if len(corners) > 0:
+                # マーカーごとに処理
+            
+            for i, corner in enumerate(corners):
+                rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corner, self.marker_length, self.camera_matrix, self.distortion_coeff)  #マーカーごとに外部パラメータ(回転ベクトルと並進ベクトル)を算出
+                # 不要なaxisを除去
+                tvec = np.squeeze(tvec)
+                rvec = np.squeeze(rvec)
+                # 回転ベクトルからrodorigues(回転行列)へ変換
+                rvec_matrix = cv2.Rodrigues(rvec)
+                rvec_matrix = rvec_matrix[0] # rodoriguesから抜き出し
+                # 並進ベクトルの転置
+                transpose_tvec = tvec[np.newaxis, :].T
+                # 合成
+                proj_matrix = np.hstack((rvec_matrix, transpose_tvec))
+                # オイラー角への変換
+                euler_angle = cv2.decomposeProjectionMatrix(proj_matrix)[6] # [deg]
+                #不要な部分除去
+                euler = np.squeeze(euler_angle)
+
+                #print("x : " + str(tvec[0]))
+                #print("y : " + str(tvec[1]))
+                #print("z : " + str(tvec[2]))
+                #print("roll : " + str(euler_angle[0]))
+                #print("pitch: " + str(euler_angle[1]))
+                #print("yaw  : " + str(euler_angle[2]))
+                #可視化
+                draw_pole_length = self.marker_length
+                img = aruco.drawAxis(img,self.camera_matrix,self.distortion_coeff,rvec,tvec,draw_pole_length,)
+                
+                cv2.putText(img,
+                            text = f"id:{ids[i][0]} | x:{str(round(tvec[0]*100,2))} | y:{str(round(tvec[1]*100,2))} | z:{str(round(tvec[2]*100,2))}",
+                            org = (640,300+i*50),
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale = 0.5,
+                            thickness = 1,
+                            color=(255,255,0),
+                            lineType=cv2.LINE_4)
+                
+                cv2.putText(img,
+                            text = f"roll:{str(round(rvec[0],2))} | pitch:{str(round(rvec[1],2))} | yaw:{str(round(rvec[2],2))}",
+                            org = (640,320+i*50),
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale = 0.5,
+                            thickness = 1,
+                            color=(255,255,0),
+                            lineType=cv2.LINE_4)
+                #cv2.imshow('drawDetectedMarkers', img)
+                #cv2.waitKey(0)
+                #cv2.destroyAllWindows()
+                info = {'id':ids[i][0],'x':tvec[0],'y':tvec[1],'z':tvec[2],'roll':euler[0],'pitch':euler[1],'yaw':euler[2]}
+                ar_info.append(info)
+                
+            # 可視化
+            detected_img = aruco.drawDetectedMarkers(img, corners, ids, (255,0,255))
+            # cv2.imwrite("detected.jpg",detected_img)
+            # cv2.imwrite("axises.jpg",img)
+        else:
+            detected_img, ar_info = img, None
+        return detected_img, ar_info
+    
+    def show(self, img):
+        cv2.imshow('realtime',img)
---
+++ AR_proto/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+from cv2 import aruco
+import sys
+import time
+import datetime
+
+import ar_module
+
+ar = ar_module.Ar_cansat()
+while True:
+    img = ar.capture(1)
+    detected_img, ar_info = ar.detect_marker(img)
+    cv2.imshow("realtime", detected_img)
+    # ar.show(detected_img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+
+# cap = cv2.VideoCapture(0)
+# while True:
+#     ret, img = cap.read()
+#     cv2.imshow("video",img)
+#     if cv2.waitKey(10) & 0xFF == ord('q'):
+#         break
