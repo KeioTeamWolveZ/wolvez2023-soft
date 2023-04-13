fd4047563e29671ec9a37beaeda800b82e8ea17e
sd4 <masato_7121@keio.jp>
2023-04-13 19:58:24 +0900

    Add: Opencv installer

--- ap_bullseye/inst_opencv_2023.sh
+++ ap_bullseye/inst_opencv_2023.sh
@@ -10,15 +10,15 @@ sudo apt-get upgrade
 sudo pip install --upgrade pip
 
 #OpenCVと依存関係のあるライブラリのインストール
-sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-100 #libhdf5-100のインストールがうまくいかないときは、libhdf5-103
-#sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103 #libhdf5-103のインストールがうまくいかないときは、libhdf5-100
+#sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-100 #libhdf5-100のインストールがうまくいかないときは、libhdf5-103
+sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103 #libhdf5-103のインストールがうまくいかないときは、libhdf5-100
 sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
 sudo apt-get install libatlas-base-dev
 sudo apt-get install libjasper-dev
 
 #Opencvnoインストール（あえてバージョンを落としてバグらないようにする）
-sudo pip3 --default-timeout=1000 install opencv-python==4.1.0.25 -qq
-sudo pip3 install opencv-contrib-python==4.1.0.25 -qq
+sudo pip3 --default-timeout=1000 install opencv-python==4.7.0.72 -qq
+sudo pip3 install opencv-contrib-python==4.7.0.72 -qq
 #エラーになるときはインストールできるバージョンを探す
 #sudo pip3 install opencv-python==
 
