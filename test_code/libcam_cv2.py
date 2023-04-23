d52e40b4229be549a8c555000ff11ba745bffeeb
Masato <masato_7121@keio.jp>
2023-04-23 16:58:13 +0900

    Edit: libcamera python file

--- test_code/libcam_cv2.py
+++ test_code/libcam_cv2.py
@@ -10,9 +10,10 @@ picam2.configure(config)
 
 picam2.start()
 #Libcamera's setting to use AF mode
-picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+#picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
 #Libcamera's setting to use AF mode (AFSpeed Fast)
-#picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous,"AFSeed":controls.AfSpeedEnum.Fast})
+#picam2.set_controls({"AFSeed":controls.AfSpeedEnum.Fast})
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous,"AFSeed":controls.AfSpeedEnum.Fast})
 
 #Display for input data in real -time 
 while True:
@@ -22,8 +23,8 @@ while True:
   key = cv2.waitKey(1)
   #If you push "esc-key", this roop is finished.
   if key == 27:
+    cv2.imwrite("test_cv2.jpg", im)
     break
-
 picam2.stop()
 cv2.destroyAllWindows()
 
