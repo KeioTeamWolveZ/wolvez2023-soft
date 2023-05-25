9f9272067a28c2ecc9bcf0e6b967774ec2f03675
Masato <masato_7121@keio.jp>
2023-05-25 14:14:21 +0900

    Adding: pkgs

--- AR_proto/os_combine/mission_pkg/__init__.py
+++ AR_proto/os_combine/mission_pkg/__init__.py
@@ -2,6 +2,6 @@ import ar_module
 import libcam_module
 import dubins_module
 
-__all__ = [ar_module,libcam_module,dubins_module]
+__all__ = ["ar_module","libcam_module","dubins_module"]
 
 print("\nImport required modules...")
