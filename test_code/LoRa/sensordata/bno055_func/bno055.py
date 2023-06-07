ce57c3fd6d75847a7ad2156597eb84cd98ed9c36
Masato <masato_7121@keio.jp>
2023-06-07 16:33:48 +0900

    Add: sensor data sender

---
+++ test_code/LoRa/sensordata/bno055_func/bno055.py
@@ -0,0 +1,40 @@
+import time
+import RPi.GPIO as GPIO
+import sys
+import numpy as np
+import bno055_conf
+
+
+class bno:
+      def __init__(self):
+            GPIO.setmode(GPIO.BCM) #GPIOの設定
+            self.bno055 = self.bno055.BNO055()
+            self.bno055.setupBno()
+            
+      def get_data(self):
+            if self.bno055.begin() is not True:
+                  print("Error initializing device")
+                  exit()
+            self.bno055.bnoread()
+            self.bno055.ax=round(self.bno055.ax,3)
+            self.bno055.ay=round(self.bno055.ay,3)
+            self.bno055.az=round(self.bno055.az,3)
+            self.bno055.gx=round(self.bno055.gx,3)
+            self.bno055.gy=round(self.bno055.gy,3)
+            self.bno055.gz=round(self.bno055.gz,3)
+            self.bno055.ex=round(self.bno055.ex,3)
+            self.bno055.ey=round(self.bno055.ey,3)
+            self.bno055.ez=round(self.bno055.ez,3)
+            accel="ax="+str(self.bno055.ax)+","\
+                  +"ay="+str(self.bno055.ay)+","\
+                  +"az="+str(self.bno055.az)
+            grav="gx="+str(self.bno055.gx)+","\
+                  +"gy="+str(self.bno055.gy)+","\
+                  +"gz="+str(self.bno055.gz)
+            euler="ex="+str(self.bno055.ex)+","\
+                  +"ey="+str(self.bno055.ey)+","\
+                  +"ez="+str(self.bno055.ez)
+            print(euler) 
+            
+            # time.sleep(0.5)
+            return {"acceleration":accel, "gravity":grav, "orientation":euler}
---
+++ test_code/LoRa/sensordata/main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import sys
+import time
+
+import gps_func.gps as gps
+import gps_func.micropyGPS as micropyGPS
+import bno055_func.bno055 as bno055
+import lora_send
+import lora_recv
+
+
+def combine_data(states=1,bno_data=1,gps_data=1): #通信モジュールの送信を行う関数
+    datalog = \
+              + "Test-state :" + states + ","\
+			  + "GPS DATA   :" + gps_data + ","\
+			  + "BNO055 DATA:" + bno_data
+    return datalog
+
+if __name__ == '__main__':
+    lora_device = "/dev/ttyAMA1"  # ES920LRデバイス名 (UART2) 
+    channel = 15
+    lr_send = lora_send.LoraSendClass(lora_device, channel)
+    bno = bno055.bno()
+    gps_obj = micropyGPS.MicropyGPS(9,'dd') # MicroGPSオブジェクトを生成する。
+                                        # 引数はタイムゾーンの時差と出力フォーマット
+    gps.thread()
+    
+    while True:
+        try:
+            # 各データの取得
+            bno_data = bno.get_data()
+            gps_data = gps.gpsread(gps_obj)
+            
+            # データを結合して送信
+            all_data = combine_data(bno_data=bno_data,gps_data=gps_data)
+            lr_send.lora_send(all_data)
+            
+            time.sleep(1)
+            
+        except KeyboardInterrupt:
+            lr_send.sendDevice.close()
+            sys.exit()
