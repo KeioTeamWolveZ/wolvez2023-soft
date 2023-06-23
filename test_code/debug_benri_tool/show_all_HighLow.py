5ec5d31d364addab68e0d63c88abe8f3cca47314
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-06-23 19:50:47 +0900

    add: HL debugger

---
+++ test_code/debug_benri_tool/show_all_HighLow.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import numpy as np
+
+GPIO.setmode(GPIO.BCM)
+valid_IO=[]
+
+for i in np.arange(1,30):
+    try:
+        GPIO.setup(i,GPIO.IN)
+        valid_IO.append(i)
+    except:
+        pass
+
+print(f"valid IO no. --> {valid_IO}")
+
+while True:
+    HL_data=[]
+    for i in valid_IO:
+        HL_data.append(GPIO.input(i))
+    print(f"PIN : {valid_IO}")
+    print(f"H/L : {HL_data}")
+
+    
