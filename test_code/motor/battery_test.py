178ea35b5177d3386ab6bb283fde952d9c1e971f
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2023-06-25 18:09:02 +0900

    battery test

--- test_code/motor/battery_test.py
+++ test_code/motor/battery_test.py
@@ -1,29 +1,36 @@
 import motor
 import RPi.GPIO as GPIO
 import time 
+import numpy as np
 from datetime import datetime
+import constant as ct
+import lora_send_onlyOnce
 
+lora_device = "/dev/ttyAMA1"
+channel = 15
 GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+lr_send = lora_send_onlyOnce.LoraSendClass(lora_device, channel)
+
 Motor1 = motor.motor(6,5,13)
 Motor2 = motor.motor(20,16,12)
 
-time.sleep(30*60)
+for i in np.arange(0,30*60):
+    lr_send.lora_send("wait for run")
+    print(f"message sent at {time.time()}")
+    time.sleep(1)
+
 
 try:
-    print("motor run") 
-#     Motor1.go(100)
-#     Motor2.go(100)
-#     Motor1.back(80)
-#     Motor2.go(80)
-#     time.sleep(0.5)
+    print("motor run")
     Motor1.go(70)
     Motor2.go(70)
-    #Motor2.back(90)
-    #time.sleep(1.08)
     i=0
     while i < 60*120/5:
         i+=1
         print("current time: "+str(datetime.now()))
+        lr_send.lora_send(f"run count:{5*i}s")
         time.sleep(5)
 
 
---
+++ test_code/motor/lora_send_onlyOnce.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import time
+import lora_setting
+
+
+# LoRa送信用クラス
+class LoraSendClass:
+
+    def __init__(self, lora_device, channel):
+        self.sendDevice = lora_setting.LoraSettingClass(lora_device)
+        self.channel = channel
+
+    # ES920LRデータ送信メソッド
+    def lora_send(self,msg=""):
+        # LoRa初期化
+        self.sendDevice.reset_lora()
+        # LoRa設定コマンド
+        set_mode = ['1', 'd', self.channel, 'e', '0001', 'f', '0002', 'g', '0001',
+                    'n', '2', 'l', '2', 'p', '1', 'y', 'z']
+#         # LoRa設定
+#         self.sendDevice.setup_lora(set_mode)
+        # LoRa(ES920LR)データ送信
+        try:
+            # 送るデータ
+            data = f'00010002-{time.time():.1f} {msg}'
+            print(f'<-- SEND -- [{data}]')
+            self.sendDevice.cmd_lora(data)
+            time .sleep(1)
+            
+        except KeyboardInterrupt:
+            self.sendDevice.close()
+        # 5秒待機
