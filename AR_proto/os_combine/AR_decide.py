6a74ecfff232f47e911bdb26d3b97b7fe5786059
Ota <ota50ko@keio.jp>
2023-07-11 15:23:37 +0900

    AR_decide

---
+++ AR_proto/os_combine/AR_decide.py
@@ -0,0 +1,22 @@
+class AR_ID_decider():
+    def __init__(self):
+        self.aprc_AR = False
+        self.omote_ura = None
+
+    def AR_decide(self, ar_info, connecting_state):
+        if connecting_state == 0:
+            if "1" in ar_info.keys() and "2" in ar_info.keys():
+                self.aprc_AR = True
+            else: 
+                self.aprc_AR = False
+
+        elif connecting_state == 1:
+            if "4" in ar_info.keys() or "4" in ar_info.keys():
+                self.aprc_AR = True
+                self.omote_ura = 0
+            elif "6" in ar_info.keys() or "7" in ar_info.keys():
+                self.aprc_AR = True
+                self.omote_ura = 1
+            else: 
+                self.aprc_AR = False
+        return {"AR":self.aprc_AR, "side":self.omote_ura}
