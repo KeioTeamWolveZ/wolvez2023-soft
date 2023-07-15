a72d105e7fe0d462255abd0283dfe70830eeace6
masato1004 <masato_7121@keio.jp>
2023-07-15 16:34:08 +0900

    Complete jitter

--- test_code/EtoE/arm.py
+++ test_code/EtoE/arm.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import pigpio as pg
 #import wiringpi as wp
 import sys
 import time
@@ -10,6 +11,7 @@ class Arm():
 		self.servo_pin = servo_pin
 		
 	def setup(self):
+		GPIO.setwarnings(False)
 		GPIO.setmode(GPIO.BCM)
 		self.mode = GPIO.getmode()
 		# print(mode)
@@ -18,16 +20,25 @@ class Arm():
 		self.pwm.start(0)
 
 	def up(self,buff=0):
+		GPIO.output(self.servo_pin,GPIO.HIGH)
 		self.pwm.ChangeDutyCycle(8+buff)
-		time.sleep(0.5)
+		time.sleep(5)
+		GPIO.output(self.servo_pin,GPIO.LOW)
+		time.sleep(0.1)
 	
 	def down(self,buff=0):
+		GPIO.output(self.servo_pin,GPIO.HIGH)
 		self.pwm.ChangeDutyCycle(5.5+buff)
-		time.sleep(0.5)
+		time.sleep(5)
+		GPIO.output(self.servo_pin,GPIO.LOW)
+		time.sleep(0.1)
 
 	def move(self,ref):
+		GPIO.output(self.servo_pin,GPIO.HIGH)
 		self.pwm.ChangeDutyCycle(ref)
 		time.sleep(0.2)
+		GPIO.output(self.servo_pin,GPIO.LOW)
+		time.sleep(0.1)
 		
 	def stop(self):
 		self.pwm.stop(0)
@@ -35,47 +46,57 @@ class Arm():
 	def calibration(self):
 		return
 
-# class ArmHardPwm():
-# 	def __init__(self,servo_pin):
-# 		self.servo_pin = servo_pin
-# 		
-# 	def setup(self):
-# 		wp.wiringPiSetupGpio()
-# 		wp.pinMode(self.servo_pin,wp.GPIO.PWM_OUTPUT)
-# 		wp.pwmSetMode(wp.GPIO.PWM_MODE_MS)
-# 		wp.pwmSetRange(1024)
-# 		wp.pwmSetClock(375)
-
-# 	def up(self,buff=0):
-# 		wp.pwmWrite(self.servo_pin,110)
-# 		time.sleep(0.5)
-	
-# 	def down(self,buff=0):
-# 		wp.pwmWrite(self.servo_pin,74)
-# 		time.sleep(0.5)
-# 		pass
-
-# 	def move(self,ref):
-# 		pass
+class ArmPg():
+	def __init__(self,servo_pin):
+		self.servo_pin = servo_pin
 		
-# 	def stop(self):
-# 		pass
+	def setup(self):
+		self.pi = pg.pi()
+		self.pi.set_mode(self.servo_pin,pg.OUTPUT)
 
-# 	def calibration(self):
-# 		return
+	def up(self,buff=0):
+		self.pi.write(self.servo_pin,1)
+		self.pi.set_servo_pulsewidth(self.servo_pin,1700)
+		time.sleep(0.5)
+		self.pi.set_servo_pulsewidth(self.servo_pin,0)
+		self.pi.write(self.servo_pin,0)
+
+	def down(self,buff=0):
+		self.pi.write(self.servo_pin,1)
+		self.pi.set_servo_pulsewidth(self.servo_pin,1400)
+		time.sleep(0.5)
+		self.pi.set_servo_pulsewidth(self.servo_pin,0)
+		self.pi.write(self.servo_pin,0)
+
+	def move(self,ref):
+		self.pi.write(self.servo_pin,1)
+		self.pi.set_servo_pulsewidth(self.servo_pin,ref)
+		time.sleep(0.5)
+		self.pi.set_servo_pulsewidth(self.servo_pin,0)
+		self.pi.write(self.servo_pin,0)
+		
+	def stop(self):
+		self.pi.stop()
+
+	def calibration(self):
+		return
 
 if __name__ == "__main__":
-	#arm = ArmHardPwm(16)
-	arm = Arm(23)
+	arm = ArmPg(23)
+	#arm = Arm(23)
 	arm.setup()
 	arm.up()
 	arm.down()
+	time.sleep(5)
 	start = time.time()
+	for i in range(1400,1700,15):
+		arm.move(i)
+		time.sleep(0.5)
 	while True:
 		end = time.time()
 		print("wating")
 		if end-start > 5:
 			break
-	GPIO.cleanup()
+	#GPIO.cleanup()
 	
 		
