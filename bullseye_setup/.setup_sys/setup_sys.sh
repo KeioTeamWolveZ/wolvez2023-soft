1d82c863a708c1549142c27ce24e0251172047b6
masato1004 <masato_7121@keio.jp>
2023-07-19 13:17:24 +0900

    Fix requirements

--- bullseye_setup/.setup_sys/setup_sys.sh
+++ bullseye_setup/.setup_sys/setup_sys.sh
@@ -1,8 +1,6 @@
 # Setting in systemctl
 # Last update: 2023/7/19 Masato Inoue
 
-# turn on pigpiod
-sudo systemctl enable pigpiod
 
 # turn pins off
 cp ./turn_off_pins.py /home/wolvez2023/Desktop/
@@ -21,3 +19,9 @@ EOF
 
 sudo systemctl enable pin_off.service
 sudo systemctl start pin_off.service
+
+# turn on pigpiod
+sudo systemctl enable pigpiod
+sudo systemctl start pigpiod
+
+echo -e "Turned on pin off service"
