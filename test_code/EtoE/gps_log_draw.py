15801353ee9d497df4c0844fe8797fe00997449f
Masato <masato_7121@keio.jp>
2023-07-25 13:45:39 +0900

    Edit: logger

--- test_code/EtoE/gps_log_draw.py
+++ test_code/EtoE/gps_log_draw.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from datetime import datetime as dt
 
 def gpslogger(start_time):
     filename = f'results/{start_time}/control_result.txt'
@@ -13,13 +14,16 @@ def gpslogger(start_time):
 
     Lat = [round(float(s[4:]),4) for s in lines if 'Lat' in s]
     Lng = [round(float(s[4:]),4) for s in lines if 'Lng' in s]
-    time = [s[5:] for s in lines if 'Time' in s]
+    time = [dt.strptime(s[5:], '%H:%M:%S') for s in lines if 'Time' in s]
+    print(time[-1]-time[0])
     
     plt.figure()
     plt.plot(Lng,Lat)
+    plt.scatter(Lng[0],Lat[0],marker='s',s=30,color='black')
+    plt.scatter(Lng[-1],Lat[1],marker='*',s=30,color='b')
     plt.xlabel("Lng")
     plt.ylabel("Lon")
-    plt.title("GPS log: "+start_time)
+    plt.title("GPS log: " + start_time + "\nTime of working: " + str(time[-1]-time[0]))
     plt.show()
     
 if __name__ == '__main__':
---
+++ test_code/EtoE/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+marker='o',s=0.5,color='black')
+    plt.scatter(Lng[-1],Lat[1],ma
