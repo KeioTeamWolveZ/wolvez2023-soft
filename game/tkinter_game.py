69a92a29a8d7952db6a3bf31ff209d137974ba7e
masato1004 <masato_7121@keio.jp>
2023-08-10 20:06:12 +0900

    :feature: shosetsu na arekore

--- game/tkinter_game.py
+++ game/tkinter_game.py
@@ -46,6 +46,14 @@ class Tkmain():
         self.style.configure("office.TButton", font=20, anchor="s")
         self.val.set(50)
 
+        # textbox
+        self.textbox = ttk.Label(
+            self.frame,
+            text='( ◜ω◝ )',
+            width=10)
+            # command=lambda: print('val:%4d' % self.val.get()))
+        self.textbox.grid(row=5, column=2, padx=5, sticky=(E))
+
         # スケールの作成
         self.val_arm = DoubleVar()
         self.sc_arm = ttk.Scale(
@@ -55,7 +63,7 @@ class Tkmain():
             length=200,
             from_=1650,
             to=950,
-            command=lambda e: print('ARM HEIGHT:%4d' % self.val_arm.get()))
+            command=self.hset)
         self.sc_arm.grid(row=2, column=4, sticky=(N, E, S, W))
         self.style = ttk.Style()
         self.style.configure("office.TButton", font=20, anchor="s")
@@ -81,6 +89,26 @@ class Tkmain():
             # command=lambda: print('val:%4d' % self.val.get()))
         self.slower.grid(row=4, column=0, padx=5, sticky=(E))
 
+        # Button (Added by kazu)
+        self.upward = ttk.Button(
+            self.frame,
+            text='上げる',
+            width=10,
+            style="office.TButton",
+            command=lambda: self.hup())
+            # command=lambda: print('val:%4d' % self.val.get()))
+        self.upward.grid(row=0, column=4, padx=5, sticky=(E))
+
+        # Button (Added by kazu)
+        self.downward = ttk.Button(
+            self.frame,
+            text='下げる',
+            width=10,
+            style="office.TButton",
+            command=lambda: self.hdown())
+            # command=lambda: print('val:%4d' % self.val.get()))
+        self.downward.grid(row=4, column=4, padx=5, sticky=(E))
+        
         # Button
         self.go = ttk.Button(
             self.frame,
@@ -89,8 +117,18 @@ class Tkmain():
             style="office.TButton",
             command=lambda: self.__go())
             # command=lambda: print('val:%4d' % self.val.get()))
-        self.go.grid(row=1, column=2, padx=5, sticky=(E))
+        self.go.grid(row=0, column=2, padx=5, sticky=(E))
 
+        # Button
+        self.go_a_little = ttk.Button(
+            self.frame,
+            text='少し走る',
+            width=10,
+            style="office.TButton",
+            command=lambda: self.__go_a_little())
+            # command=lambda: print('val:%4d' % self.val.get()))
+        self.go_a_little.grid(row=1, column=2, padx=5, sticky=(E))
+        
         # Button
         self.back = ttk.Button(
             self.frame,
@@ -131,19 +169,82 @@ class Tkmain():
             # command=lambda: print('val:%4d' % self.val.get()))
         self.grasp.grid(row=2, column=2, padx=5, sticky=(E))
 
+        # Button (added by kazu)
+        self.picture = ttk.Button(
+            self.frame,
+            text='写真',
+            width=10,
+            style="office.TButton",
+            command=lambda: self.__picture())
+            # command=lambda: print('val:%4d' % self.val.get()))
+        self.picture.grid(row=1, column=3, padx=5, sticky=(E))
+
+        # Button (added by kazu)
+        self.dance = ttk.Button(
+            self.frame,
+            text='踊る',
+            width=10,
+            style="office.TButton",
+            command=lambda: self.__dance())
+            # command=lambda: print('val:%4d' % self.val.get()))
+        self.dance.grid(row=1, column=1, padx=5, sticky=(E))
+
+
+        # Button (added by kazu)
+        self.finish = ttk.Button(
+            self.frame,
+            text='終わる',
+            width=10,
+            style="office.TButton",
+            command=lambda: self.__finish())
+            # command=lambda: print('val:%4d' % self.val.get()))
+        self.finish.grid(row=3, column=3, padx=5, sticky=(E))
+
     def vup(self):
         if self.val.get() < 90:
             self.val.set(self.val.get() + 10)
         else:
             self.val.set(100)
         print('速度:%4d' % self.val.get())
+        self.textbox["text"]='速度:%4d' % self.val.get()
             
     def vdown(self):
-        if self.val.get() > 50:
-            print(self.val.get())
+        if self.val.get() > 60:
+            self.val.set(self.val.get() - 10)
         else:
             self.val.set(50)
         print('速度:%4d' % self.val.get())
+        self.textbox["text"]='速度:%4d' % self.val.get()
+
+    def hup(self):
+        if self.val_arm.get() < 1600:
+            self.val_arm.set(self.val_arm.get() + 50)
+        else:
+            self.val_arm.set(1650)
+        print('高さ:%4d' % self.val_arm.get())
+        self.textbox["text"]='wait a second'
+        self.arm.move(self.val_arm.get())
+        self.textbox["text"]='高さ:%4d' % self.val_arm.get()
+            
+    def hdown(self):
+        if self.val_arm.get() > 1000:
+            self.val_arm.set(self.val_arm.get() - 50)
+        else:
+            self.val_arm.set(950)
+        print('高さ:%4d' % self.val_arm.get())
+        self.textbox["text"]='wait a second'
+        self.arm.move(self.val_arm.get())
+        self.textbox["text"]='高さ:%4d' % self.val_arm.get()
+
+    def hset(self,var):
+        try:
+            value=int(var[:4])
+        except ValueError:
+            value=int(var[:3])
+        self.val_arm.set(value)
+        self.arm.move(value)
+        self.textbox["text"]='高さ:%4d' % self.val_arm.get()
+
     
     def __arm_updown(self):
         # if not self.__arm:
@@ -154,12 +255,18 @@ class Tkmain():
             # self.__arm = False
             # print("ARM DOWN!")
             # self.arm.down()
-        self.arm.move(self.val_arm.get())
+        self.textbox["text"]='!ATTACK!'
+        self.textbox.update()
+        self.arm.down()
+        time.sleep(1)
+        self.arm.up()
+        self.textbox["text"]='高さ:%4d' % self.val_arm.get()
 
     def __go(self):
         if not self.__run:
             print("RUN!")
             self.__run = True
+            self.textbox["text"]='RUN 速度:%4d' % self.val.get()
             self.MotorR.go(float(self.val.get()))
             self.MotorL.go(float(self.val.get()))
         else:
@@ -167,8 +274,20 @@ class Tkmain():
             self.__run = False
             self.MotorR.stop()
             self.MotorL.stop()
+            self.textbox["text"]='STOP!'
         # self.arm.up()
         self.arm.move(self.val_arm.get())
+
+    def __go_a_little(self):
+        print("RUN just a little bit!")
+        self.MotorR.go(60)
+        self.MotorL.go(60)
+        time.sleep(0.05)
+        self.MotorR.stop()
+        self.MotorL.stop()
+        # self.arm.up()
+        self.arm.move(self.val_arm.get())
+        self.textbox["text"]='少し走る'
 
     def __back(self):
         print("BACK!")
@@ -179,6 +298,7 @@ class Tkmain():
         self.MotorL.stop()
         # self.arm.up()
         self.arm.move(self.val_arm.get())
+        self.textbox["text"]='うしろ'
 
     def __right(self):
         print("TURN RIGHT!")
@@ -189,6 +309,7 @@ class Tkmain():
         self.MotorL.stop()
         # self.arm.up()
         self.arm.move(self.val_arm.get())
+        self.textbox["text"]='みぎ'
 
     def __left(self):
         print("TURN LEFT!")
@@ -199,6 +320,71 @@ class Tkmain():
         self.MotorL.stop()
         # self.arm.up()
         self.arm.move(self.val_arm.get())
+        self.textbox["text"]='ひだり'
+        
+    def __finish(self):
+        print("finish the game!")
+        self.MotorR.stop()
+        self.MotorL.stop()
+        # self.arm.up()
+        self.arm.move(self.val_arm.get())
+        self.root.destroy()
+        
+    def __dance(self):
+        print("ଘ (੭*ˊᵕˋ)੭*")
+        self.textbox["text"]='ଘ (੭*ˊᵕˋ)੭*'
+        self.textbox.update()
+        self.arm.down()
+        time.sleep(0.8)
+        print("٩(ˊᗜˋ*)و")
+        self.textbox["text"]='٩(ˊᗜˋ*)و'
+        self.textbox.update()
+        self.arm.up()
+        time.sleep(0.8)
+        print("ଘ (੭*ˊᵕˋ)੭*")
+        self.textbox["text"]='ଘ (੭*ˊᵕˋ)੭*'
+        self.textbox.update()
+        self.arm.down()
+        time.sleep(0.8)
+        print("٩(ˊᗜˋ*)و")
+        self.textbox["text"]='٩(ˊᗜˋ*)و'
+        self.textbox.update()
+        self.arm.up()
+        time.sleep(0.8)
+        print("(‘ω’ )三")
+        self.textbox["text"]='(‘ω’ )三'
+        self.textbox.update()
+        self.MotorR.back(float(self.val.get()))
+        self.MotorL.go(float(self.val.get()))
+        time.sleep(1.5)
+        print("三( ‘ω’)")
+        self.textbox["text"]='三( ‘ω’)'
+        self.textbox.update()
+        self.MotorR.go(float(self.val.get()))
+        self.MotorL.back(float(self.val.get()))
+        time.sleep(1.5)
+        print("(‘ω’ )三")
+        self.textbox["text"]='(‘ω’ )三'
+        self.textbox.update()
+        self.MotorR.back(float(self.val.get()))
+        self.MotorL.go(float(self.val.get()))
+        time.sleep(1.5)
+        print("三( ‘ω’)")
+        self.textbox["text"]='三( ‘ω’)'
+        self.textbox.update()
+        self.MotorR.go(float(self.val.get()))
+        self.MotorL.back(float(self.val.get()))
+        time.sleep(1.5)
+        self.MotorR.stop()
+        self.MotorL.stop()
+        print("ヾ(*´∀｀*)ﾉ ")
+        self.textbox["text"]='ヾ(*´∀｀*)ﾉ '
+        self.textbox.update()
+        
+    def __picture(self):
+        print("This function will be implemented by Yuma soon (｢･ω･)｢")
+        self.textbox["text"]='(｢･ω･)｢ '
+        self.textbox.update()
         
     def tkstart(self):
         self.root.mainloop()
